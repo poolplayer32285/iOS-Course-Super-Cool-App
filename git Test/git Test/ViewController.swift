227199fc052da4ed40237b94b38430e122d69e61
Charles Hamblen <Chamblen@steelman-js.com>
2016-03-31 16:05:31 -0700

    starting with github

---
+++ git Test/git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git Test
+//
+//  Created by Charles Hamblen on 3/31/16.
+//  Copyright © 2016 Charles Hamblen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
